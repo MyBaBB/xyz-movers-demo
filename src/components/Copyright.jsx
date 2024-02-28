07e36f65fd89b032d11254f3c7e566fccf1220f5
Brett Baker <GitBaBB@MyBaBB.com>
2024-02-28 05:32:53 -0800

    footer date year wrong

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -7,7 +7,7 @@ function Copyright  () {
 
 <p className="fixed bottom-0 right-5 whitespace-nowrap text-xs">
   <span className="text-varBoneWhite dark:text-varLIGHTWING text-[7px] sm:text-[9px] z-0">
-    &lt;&lt;&lt;&copy;MyBaBB <em>12-2013</em>&gt;&gt;&gt;
+    &lt;&lt;&lt;&copy;MyBaBB <em>12-2023</em>&gt;&gt;&gt;
   </span>
 </p>
 
